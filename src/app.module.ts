6ff357f0c9a4f23757330a29c6c6d8cee6c8ddd3
Ron Almog <ron.almog@capco.com>
2019-08-10 17:54:18 -0400

    add module, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/customers', {useNewUrlParser: true}),
+    CustomerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
